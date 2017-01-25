dfb3692b35e429afe0b2f96320da11f1f1b6972d
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-25 17:01:49 +0400

    Класс, инкапсулирующий в себе один RssStream

---
+++ RssClient/app/src/main/java/ru/rychagov/rssclient/data/RssStream.java
@@ -0,0 +1,26 @@
+package ru.rychagov.rssclient.data;
+
+public class RssStream {
+
+  private int id;
+  private String title;
+  private String link;
+
+  public RssStream(int id, String title, String link) {
+    this.id = id;
+    this.title = title;
+    this.link = link;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getLink() {
+    return link;
+  }
+}
