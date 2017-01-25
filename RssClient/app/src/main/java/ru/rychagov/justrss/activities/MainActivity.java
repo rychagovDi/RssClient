a3722b5b82c1e881639fd906620083b4220e86eb
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-25 22:30:24 +0400

    Добавлено меню в Toolbar.

--- RssClient/app/src/main/java/ru/rychagov/justrss/activities/MainActivity.java
+++ RssClient/app/src/main/java/ru/rychagov/justrss/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ru.rychagov.justrss.R;
 
@@ -17,15 +19,34 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.a_main);
 
     context = getApplicationContext();
+    setToolbar("hello");
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.m_main, menu);
+    return true;
+  }
 
-  // --- UI methods --- //
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+// --- UI methods --- //
 
   private void setToolbar(String title) {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     toolbar.setTitle(title);
     toolbar.setTitleTextColor(getResources().getColor(R.color.white_color));
     setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
   }
 }
