8322096f6559dff8a5793ec9c8a754f018b6de4a
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-25 15:23:45 +0400

    Создание таблицы rss_entry

--- RssClient/app/src/main/java/ru/rychagov/rssclient/data/DataBaseHelper.java
+++ RssClient/app/src/main/java/ru/rychagov/rssclient/data/DataBaseHelper.java
@@ -19,8 +19,17 @@ public class DataBaseHelper  extends SQLiteOpenHelper {
 
     String SQL_CREATE_TABLE = "CREATE TABLE " + RssStreams.TABLE_NAME + " ("
             + RssStreams._ID + " INTEGER NOT NULL, "
-            + RssStreams.RSS_TITLE + " TEXT NOT NULL, "
-            + RssStreams.RSS_LINK + " TEXT NOT NULL);";
+            + RssStreams.COLUMN_RSS_TITLE + " TEXT NOT NULL, "
+            + RssStreams.COLUMN_RSS_LINK + " TEXT NOT NULL);";
+
+    db.execSQL(SQL_CREATE_TABLE);
+
+    SQL_CREATE_TABLE = "CREATE TABLE " + RssEntry.TABLE_NAME + " ("
+            + RssEntry._ID + " INTEGER NOT NULL, "
+            + RssEntry.COLUMN_TITLE + " TEXT NOT NULL, "
+            + RssEntry.COLUMN_DESCR + " TEXT NOT NULL, "
+            + RssEntry.COLUMN_LINK + " TEXT NOT NULL, "
+            + RssEntry.COLUMN_PUBDATE + " TEXT NOT NULL);";
 
     db.execSQL(SQL_CREATE_TABLE);
   }
@@ -33,7 +42,16 @@ public class DataBaseHelper  extends SQLiteOpenHelper {
   public static class RssStreams implements BaseColumns {
     public static final String TABLE_NAME = "rss_streams";
     public static final String _ID = BaseColumns._ID;
-    public static final String RSS_TITLE = "rss_title";
-    public static final String RSS_LINK = "rss_link";
+    public static final String COLUMN_RSS_TITLE = "rss_title";
+    public static final String COLUMN_RSS_LINK = "rss_link";
+  }
+
+  public static class RssEntry implements BaseColumns {
+    public static final String TABLE_NAME = "rss_entry";
+    public static final String _ID = BaseColumns._ID;
+    public final static String COLUMN_TITLE = "title";
+    public final static String COLUMN_DESCR = "description";
+    public final static String COLUMN_LINK = "link";
+    public final static String COLUMN_PUBDATE = "date";
   }
 }
