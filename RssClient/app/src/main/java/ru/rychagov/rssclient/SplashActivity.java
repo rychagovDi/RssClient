566abf9708e856edb298f9e5a45bb034fecb3f70
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-25 12:20:18 +0400

    Переход из Splash экрана к главной активности

--- RssClient/app/src/main/java/ru/rychagov/rssclient/SplashActivity.java
+++ RssClient/app/src/main/java/ru/rychagov/rssclient/SplashActivity.java
@@ -1,7 +1,9 @@
 package ru.rychagov.rssclient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +11,20 @@ public class SplashActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.a_splash);
+
+    if (savedInstanceState == null) {
+
+      // TODO Download new RSS from internet
+
+      Handler handler = new Handler();
+      handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          startActivity(new Intent(SplashActivity.this, MainActivity.class));
+          finish();
+        }
+      }, 2000);
+    }
+
   }
 }
