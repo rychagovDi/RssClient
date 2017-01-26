c2667b8fd7209de1b0d4d9fef71b7e6d2a383e06
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-26 20:56:11 +0400

    Создал класс для фрагмента бокового меню

---
+++ RssClient/app/src/main/java/ru/rychagov/justrss/fragments/RssStreamsFragment.java
@@ -0,0 +1,8 @@
+package ru.rychagov.justrss.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class RssStreamsFragment extends Fragment{
+
+  private static final String TAG = "RssStreamsFragment";
+}
