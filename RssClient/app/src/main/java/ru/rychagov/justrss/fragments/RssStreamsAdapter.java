e503a76d81c5c470a3aac3a806e206e7a90086b0
Rychagov D.I <ruchagov-diman@mail.ru>
2017-01-26 20:25:32 +0400

    Написал адаптер для RecyclerView бокового меню

--- RssClient/app/src/main/java/ru/rychagov/justrss/fragments/RssStreamsAdapter.java
+++ RssClient/app/src/main/java/ru/rychagov/justrss/fragments/RssStreamsAdapter.java
@@ -1,29 +1,72 @@
 package ru.rychagov.justrss.fragments;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ru.rychagov.justrss.R;
+import ru.rychagov.justrss.data.RssStream;
 
 public class RssStreamsAdapter extends RecyclerView.Adapter<RssStreamsAdapter.RssStreamsViewHolder> {
 
+  private static final String TAG = "RssStreamsAdapter";
+
+  private Context context;
+  private ArrayList<RssStream> streams;
+
   @Override
   public RssStreamsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+    View view = LayoutInflater.from(context).inflate(R.layout.nav_row, parent, false);
+    return new RssStreamsViewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(RssStreamsViewHolder holder, int position) {
-
+    RssStream stream = streams.get(position);
+    holder.title.setText(stream.getTitle());
+    holder.listener.setLink(stream.getLink());
+    holder.listener.setId(stream.getId());
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return streams.size();
   }
 
   class RssStreamsViewHolder extends RecyclerView.ViewHolder {
+    TextView title;
+    OnItemClickListener listener = new OnItemClickListener();
+
     public RssStreamsViewHolder(View itemView) {
       super(itemView);
+      title = (TextView) itemView.findViewById(R.id.nav_row_title);
+      itemView.setOnClickListener(listener);
+    }
+  }
+
+  private class OnItemClickListener implements View.OnClickListener {
+
+    private String link;
+    private int id;
+
+    @Override
+    public void onClick(View v) {
+      // TODO broadcast
+
+      Log.d(TAG, String.format("link: %s, id: %d", link, id));
+    }
+
+    public void setLink(String link) {
+      this.link = link;
+    }
+    public void setId(int id) {
+      this.id = id;
     }
   }
 }
